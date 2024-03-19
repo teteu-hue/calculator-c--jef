45065078f8a4b8fed1066b256de14de56ba9a12a
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2024-03-18 21:46:16 -0300

    calculadora

---
+++ Calculadora4op/Calculadora.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora4op
+{
+    internal class Calculadora
+    {
+
+        public int Soma()
+        {
+            Console.WriteLine("********** SOMA **********");
+            Console.WriteLine("Digite o primeiro número: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int result = num1 + num2;
+
+            Console.WriteLine("Resultado do calculo: {0}", result);
+
+            return result;
+        }
+        public int Subtracao()
+        {
+
+            Console.WriteLine("********** SUBTRAÇÃO **********");
+            Console.WriteLine("Digite o primeiro número: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int result = num1 - num2;
+
+            Console.WriteLine("Resultado do calculo: {0}", result);
+
+            return result;
+
+        }
+        public int Multiplicacao()
+        {
+
+            Console.WriteLine("********** MULTIPLICAÇÃO **********");
+            Console.WriteLine("Digite o primeiro número: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (num1 == 0 || num2 == 0)
+            {
+                Console.WriteLine("Toda multiplicação por zero é zero");
+                return 0;
+            }
+
+            int result = num1 * num2;
+
+            Console.WriteLine("Resultado do calculo: {0}", result);
+
+            return result;
+        }
+        public double Divisao()
+        {
+
+            Console.WriteLine("********** DIVISÃO **********");
+            Console.WriteLine("Digite o primeiro número: ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            if (num1 == 0 || num2 == 0)
+            {
+                Console.WriteLine("Toda divisão por zero é zero");
+                return 0;
+            }
+
+            double result = num1 / num2;
+
+            Console.WriteLine("Resultado do calculo: {0}", result);
+
+            return result;
+
+        }
+    }
+}
---
+++ Calculadora4op/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Calculadora4op
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo a calculadora do Teteu e da Dede");
+
+            Calculadora calculadora = new Calculadora();
+
+            int opcao = 0;
+
+            do
+            {
+                Console.WriteLine("Qual calculo você precisa fazer?");
+                Console.WriteLine("Digite as Opções: ");
+                Console.WriteLine("==============================");
+                Console.WriteLine("1 - Soma");
+                Console.WriteLine("2 - Subtração");
+                Console.WriteLine("3 - Multiplicação");
+                Console.WriteLine("4 - Divisão");
+                Console.WriteLine("0 - Sair");
+
+                opcao = int.Parse(Console.ReadLine());
+
+                switch(opcao)
+                {
+
+                    case 1:
+                        calculadora.Soma();
+                        break;
+
+                    case 2:
+                        calculadora.Subtracao();
+                        break;
+
+                    case 3:
+                        calculadora.Multiplicacao();
+                        break;
+
+                    case 4:
+                        calculadora.Divisao();
+                        break;
+                }
+
+            } while (opcao != 0);
+            
+        }
+    }
+}
